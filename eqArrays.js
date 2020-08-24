c9b5bc350ced3cd10156efc12424c6912c87d499
affaf <affaf.rai@hotmail.com>
2020-08-24 18:16:32 +0000

    modify eqArray to test file

--- eqArrays.js
+++ eqArrays.js
@@ -1,18 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ assertion passed : ${actual} ===  ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 assertion failed : ${actual} !== ${expected}`);
-  }
-};
-
-//assertEqual("Lighthouse Labs", "Bootcamp");
-//assertEqual(1, 1);
-
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(testArr1,testArr2) {
-
-
   if (testArr1.length === testArr2.length){
     for(let i = 0; i < testArr1.length; i++){
       if (testArr1[i] !== testArr2[i]){
@@ -22,13 +10,12 @@ const eqArrays = function(testArr1,testArr2) {
     return true;
   }
 }
-
-console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1]) )// => false
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]) )// => false
+// console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+// console.log(eqArrays([1, 2, 3], [3, 2, 1]) )// => false
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]) )// => false
 // assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 // assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => should PASS
 // assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
 // assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => should PASS
+module.exports = eqArrays;
